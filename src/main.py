1b9ab6f9d4e222b73034d1f438a11e05250c9540
CyzmiX <itzmedigamingx@gmail.com>
2026-01-18 13:32:09 +0100

    proxy

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import librosa
 import numpy as np
 import supabase
 from time import sleep
+import random
 
 from keyfinder import Tonal_Fragment
 from tagger import tag_sample
@@ -16,12 +17,40 @@ client = supabase.create_client(
     os.getenv("SUPABASE_KEY")
 )
 
+PROXIES = []
+
 samples_data = []
+
+PROXIES = [
+    'socks4://41.169.151.154:4153',
+    'socks4://198.8.94.174:39078',
+    'socks4://167.71.65.149:1080',
+    'socks4://31.43.33.55:4153',
+    'socks4://36.95.189.165:5678',
+    'socks4://46.38.41.150:58357',
+    'socks4://200.192.236.242:1080',
+    'socks4://124.248.184.144:1080',
+    'socks4://196.0.113.10:31651',
+    'socks4://171.248.216.79:1080',
+    'socks4://202.40.177.94:5678',
+    'socks4://47.238.60.156:8008',
+    'socks4://171.248.208.241:1080',
+    'socks4://202.131.159.61:5678',
+    'socks4://200.108.190.110:9800',
+    'socks4://141.94.70.195:46797',
+    'socks4://178.130.47.129:1082',
+    'socks4://49.207.177.65:5678',
+    'socks4://187.63.9.62:63253',
+    'socks4://96.36.50.99:39593',
+    'socks4://61.9.34.118:58765',
+    'socks4://40.134.10.174:18351',
+    'socks4://171.248.217.140:1080'
+]
 
 def main():
     url = "https://youtube.com/playlist?list=PLW4NDfp3IicGyAw0Ix0Z0yr-FL35Eo_qB&si=C5iAT8adl0V1M90c"
 
-    pl = Playlist(url, 'WEB')
+    pl = Playlist(url, proxies={"https": f"socks5://{random.choice(IPS)}"})
     for video in pl.videos[:40]:  
 
         ys = video.streams.get_audio_only()
